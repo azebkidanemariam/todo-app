4a14087d8f721fdeef69098454cc96abb3a517a2
Azeb.K <54040527+azebkidanemariam@users.noreply.github.com>
2021-02-09 15:09:28 +0100

    mutations
    i vuex added mutations och kopplat med todoitems.

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    todos: [],
+    showDone: false,
   },
   mutations: {
+    addTodo: (state, todo) =>
+      state.todos.push({
+        id: Date.now(),
+        content: todo,
+        done: false,
+      }),
+    removeTodo: (state, todo) =>
+      state.todos.filter((element) => element.id != todo.id),
+    showDoneDo: (state) => (state.showDone = !state.showDone),
   },
-  actions: {
+  getters: {
+    toDo: (state) =>
+      state.showDone ? state.todos.filter((e) => !e.done) : state.todos,
+    toDoLeft: (state) => state.todos.filter((todo) => !todo.done).length,
+    showHide: (state) => (state.showDone ? "hide" : "show"),
   },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
